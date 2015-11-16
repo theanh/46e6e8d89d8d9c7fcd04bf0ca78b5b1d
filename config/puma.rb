ab552d9d4b4576e67477e013a74f7b5aa36d5454
The Anh Nguyen <theanh.huy@gmail.com>
2015-11-16 23:07:01 +0700

    Clean unused code.

--- config/puma.rb
+++ config/puma.rb
@@ -9,7 +9,7 @@ port        ENV['PORT']     || 3000
 environment ENV['RACK_ENV'] || 'development'
 
 on_worker_boot do
-  # Worker specific setup for Rails 4.1+
-  # See: https://devcenter.heroku.com/articles/deploying-rails-applications-with-the-puma-web-server#on-worker-boot
-  ActiveRecord::Base.establish_connection
+  ActiveSupport.on_load(:active_record) do
+    ActiveRecord::Base.establish_connection
+  end
 end
